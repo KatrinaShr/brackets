e4e6c8b9377bf24d600f56b06daeaa4a9cabcf0c
KatrinaShr <katrineshr@gmail.com>
2024-10-20 15:01:44 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
+  let BRACKETS = {};
+  let OPEN_BRACKETS = Object.values(BRACKETS);
+  let stack = [];
 
-    let bracketsOpen = [];
-    let bracketsPair = {};
-    let stack = [];
+  bracketsConfig.forEach(element => {
+    BRACKETS[element[1]] = element[0];
+    OPEN_BRACKETS.push(element[0]);
+  });
 
-    bracketsConfig.forEach((element, index) => {
-        bracketsPair[element[1]] = element[0];
-    });
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topSymbol = stack[stack.length - 1];
 
-    bracketsOpen = Object.values(bracketsPair);
-
-    for (let i = 0; i < str.length; i++) {
-        let topElement = stack[stack.length - 1];   
-
-        if (topElement !== undefined) {
-            if (bracketsPair[str[i]] === topElement) {
-                stack.pop();
-            }
-
-            else if (bracketsOpen.includes(str[i])) {
-                stack.push(str[i]);
-            } 
-
-            else return false;
-            }
-        else if (bracketsOpen.includes(str[i])) {
-            stack.push(str[i]);
-        }  
-
-        else return false;
-
-        } 
-    return stack.length === 0;
+    if (topSymbol && BRACKETS[currentSymbol] === topSymbol) {
+      stack.pop();
+    } else {
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+      } 
+    }
+  }
+  return stack.length === 0;
 }
